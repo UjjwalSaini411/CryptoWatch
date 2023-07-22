bb1ebd94d89c6c3b15d80d6e378665e1c616a9ef
UjjwalSaini411 <ujjwal.saini@allfriends.studio>
2023-07-22 14:32:56 +0530

    layout set

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import Navbar from './components/Navbar';
+
+// import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Routes>
+            {/* <Route path="/" element={<Homepage />} />
+            <Route path="/exchanges" element={<Exchanges />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+            <Route path="/news" element={<News />} /> */}
+          </Routes>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+          Copyright © 2021
+          <NavLink to="/">Cryptoverse Inc.</NavLink>
+          <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <NavLink to="/" exact activeClassName="active-link">Home</NavLink>
+          <NavLink to="/exchanges" activeClassName="active-link">Exchanges</NavLink>
+          <NavLink to="/news" activeClassName="active-link">News</NavLink>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { NavLink } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png';
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo"><NavLink to="/">Cryptoverse</NavLink></Typography.Title>
+            </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                    <NavLink to="/">Home</NavLink>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <NavLink to="/cryptocurrencies">Cryptocurrencies</NavLink>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <NavLink to="/exchanges">Exchanges</NavLink>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <NavLink to="/news">News</NavLink>
+                </Menu.Item>
+            </Menu>
+        </div>
+    )
+}
+
+export default Navbar
