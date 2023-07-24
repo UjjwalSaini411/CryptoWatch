f2af582372d1930b8d747e78988ae3bc0d30361e
UjjwalSaini411 <ujjwal.saini@allfriends.studio>
2023-07-24 15:15:00 +0530

    created the store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'ff53217cecmsh37017d8251719dep1b193bjsn836051fd6f7c',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   }; 
